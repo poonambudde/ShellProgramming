210a579bda7d53073c6e519b43009d9183f3df09
“poonambudde” <buddepoonam123@gmail.com>
2022-01-14 19:47:44 +0530

    Added: used case statement to check multiple conditions

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 
-isPresent=1;
-randomCheck=$((RANDOM%2));
-if [ $isPresent -eq $randomCheck ];
-then
-    echo "Employee is Present";
-else
-    echo "Employee is Absent";
-fi
+isFullTime=1;
+isPartTime=2;
+empRatePerHrs=20;
+randomCheck=$((RANDOM%3));
+case $randomCheck in
+   $isFullTime
+     empHrs=8
+     ; ;
+   $isPartTime
+     empHrs=4
+     ; ;
+   *)
+     empHrs=0
+     ; ;
+esac
+
+salary=$(($empRatePerHrs*$empHrs));
+echo $salary;
