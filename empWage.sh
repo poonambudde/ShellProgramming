287471ae524de4d225f9275113a798e02dff0609
“poonambudde” <buddepoonam123@gmail.com>
2022-01-14 18:56:21 +0530

    Added: part time functionallity

--- empWage.sh
+++ empWage.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -x
 
-is FullTime=1;
-isPresent=2;
+isFullTime=1;
+isPartTime=2;
 empRatePerHrs=20;
-randomCheck=$((RANDOM%3));
+randomCheck=$((Random%3));
 if [ $isFullTime -eq $randomCheck ];
 then
     empHrs=8;
-elif [ $isPartTime -eq $randomeCheck ];
+elif [ $isPartTime -eq $randomcheck ];
 then
     empHrs=4;
 else
     empHrs=0;
 fi
+salary=$(($empRatePerHrs*$empHrs))
+echo $salary;
