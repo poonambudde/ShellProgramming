c16ecf47623d4027cf065c3eda0ac456ec97f040
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-01-17 15:56:54 +0530

    Update empWage.sh

--- empWage.sh
+++ empWage.sh
@@ -2,16 +2,29 @@
 
 isFullTime=1;
 isPartTime=2;
+totalSalary=0
 empRatePerHrs=20;
-randomCheck=$((Random%3));
-if [ $isFullTime -eq $randomCheck ];
-then
-    empHrs=8;
-elif [ $isPartTime -eq $randomcheck ];
-then
-    empHrs=4;
-else
-    empHrs=0;
-fi
-salary=$(($empRatePerHrs*$empHrs))
-echo $salary;
+maxWorkingHrs=100;
+maxWorkingDays=20;
+totalEmpHrs=0;
+while [[ $totalEmpHrs -lt $maxWorkingHrs && $totalWorkingDays -lt $maxWorkingDays ]]
+do
+   ((totalWorkingDays++))
+   empCheck=$((RANDOM%3));
+   case $empCheck in
+     $isFullTime)
+           empHrs=8
+           ;;
+     $isPartTime)
+           empHrs=4
+           ;;
+     *)
+        empHrs=0
+        ;;
+   esac
+   totalEmpHrs=$(($totalEmpHrs+$empHrs));
+
+done
+
+totalSalary=$(($totalEmpHrs*$empRatePerHrs));
+echo  $totalSalary
