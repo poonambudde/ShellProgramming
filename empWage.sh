5089769a1980b499cd6f017d0ff46fae72047b06
“poonambudde” <buddepoonam123@gmail.com>
2022-01-18 12:29:02 +0530

    Added: used function to calculate the emp hrs

--- empWage.sh
+++ empWage.sh
@@ -1,17 +1,39 @@
-#!/bin/bash -x
-
 isFullTime=1;
 isPartTime=2;
+totalSalary=0;
 empRatePerHrs=20;
-randomCheck=$((Random%3));
-if [ $isFullTime -eq $randomCheck ];
-then
-    empHrs=8;
-elif [ $isPartTime -eq $randomcheck ];
-then
-    empHrs=4;
-else
-    empHrs=0;
-fi
-salary=$(($empRatePerHrs*$empHrs))
-echo $salary;
+maxWorkingDays=20;
+maxWorkingHrs=100;
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+function getWorkingHrs(){
+	case $1 in
+      $isFullTime) 
+         empHrs=8
+         ;;
+      $isPartTime)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+   esac
+	echo $empHrs
+}
+
+function getEmpWagePerDay(){
+	echo $(($1*$empRatePerHrs))
+}
+while [[ $totalEmpHrs -lt $maxWorkingHrs && $totalWorkingDays -le $maxWorkingDays ]]
+do
+((totalWorkingDays++))
+	empCheck=$((RANDOM%3));
+	empHrs=$( getWorkingHrs $empCheck )
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	dailyWage[$totalWorkingDays]=$( getEmpWagePerDay $empHrs)
+done
+
+totalSalary=$(($totalEmpHrs * $empRatePerHrs))
+echo $totalSalary
+echo ${dailyWage[@]}
